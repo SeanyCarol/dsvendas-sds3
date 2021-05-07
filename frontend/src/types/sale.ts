f33cd5ff328dcdc639bff75ef38e9970d7bd0106
SeanyCarol <seany@ice.ufjf.br>
2021-05-07 17:18:26 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+  id: number;
+  visited: string;
+  deals: string;
+  amount: number;
+  date: string;
+  seller: Seller;
+}
+
+export type SalePage = {
+  content?: Sale[];
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  number: number;
+  numberOfElements?: number;
+  size?: number;
+  empty?: boolean;
+}
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
